1448892bcba94e19021a5017d1183d8c29383d88
antreun <antti.reunamo@helsinki.fi>
2018-03-03 18:11:19 +0200

    teht 1-7

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import Blog from './components/Blog'
+import Togglable from './components/Togglable'
+import BlogForm from './components/BlogForm'
+import Notification from './components/Notification'
 import blogService from './services/blogs'
+import loginService from './services/login'
+
 
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
+      user: null,
+      username: "",
+      password: "",
       blogs: []
     }
   }
@@ -14,13 +22,130 @@ class App extends React.Component {
     blogService.getAll().then(blogs =>
       this.setState({ blogs })
     )
-  } 
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogAppUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      this.setState({user})
+      blogService.setToken(user.token)
+    }
+  }
+
+  handleLoginFieldChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value })
+  }
+
+  logout = async (event) => {
+    window.localStorage.removeItem('loggedBlogAppUser')
+    this.setState({user:null})
+    this.notify('Kirjauduttu ulos!')
+  }
+
+  login = async (event) => {
+    event.preventDefault()
+    try{
+      const user = await loginService.login({
+        username: this.state.username,
+        password: this.state.password
+      })
+
+      this.setState({ username: '', password: '', user})
+      window.localStorage.setItem('loggedBlogAppUser', JSON.stringify(user))
+      blogService.setToken(user.token)
+      this.notify('Kirjautuminen onnistui!')
+    } catch(exception) {
+      this.notify('Käyttäjätunnus tai salasana virheellinen!')
+    }
+  }
+
+  newBlog = (event) => {
+    event.preventDefault()
+    this.blogForm.toggleVisibility()
+    try{
+
+      const blogObject = {
+        user: this.state.user,
+        title: this.state.title,
+        author: this.state.author,
+        url: this.state.url
+      }
+
+      blogService
+        .create(blogObject)
+        .then(newBlog => {
+          this.setState({
+            blogs: this.state.blogs.concat(newBlog),
+            title: '',
+            author: '',
+            url: ''
+          })
+          this.notify('Lisätty blogi \''+newBlog.title+'\', kirjoittaja: '+newBlog.author)
+        })
+  }catch(exception) {
+    this.notify('Blogin lisääminen ei onnistunut!')
+  }
+}
+
+  notify = async (message) => {
+    this.setState({
+      error: message,
+    })
+    setTimeout(() => {
+      this.setState({ error: null })
+    }, 5000)
+  }
 
   render() {
+    if (this.state.user === null) {
+      return (
+        <form onSubmit={this.login}>
+        <Notification message={this.state.error}/>
+          <div>
+            käyttäjätunnus
+            <input
+              type="text"
+              name="username"
+              value={this.state.username}
+              onChange={this.handleLoginFieldChange}
+            />
+          </div>
+          <div>
+            salasana
+            <input
+              type="password"
+              name="password"
+              value={this.state.password}
+              onChange={this.handleLoginFieldChange}
+            />
+          </div>
+          <button type="submit">kirjaudu</button>
+        </form>
+      )
+    }
+
     return (
       <div>
-        <h2>blogs</h2>
-        {this.state.blogs.map(blog => 
+
+
+        <Notification message={this.state.error}/>
+
+        <p>{this.state.user.name} logged in &nbsp;
+          <button onClick={this.logout}>Logout!</button>
+        </p>
+
+        <h2>Blogs</h2>
+
+        <Togglable buttonLabel="New Blog" ref={component => this.blogForm = component}>
+          <BlogForm
+            onSubmit={this.newBlog}
+            title={this.state.title}
+            author={this.state.author}
+            url={this.state.url}
+            handleChange={this.handleLoginFieldChange}
+          />
+        </Togglable>
+
+        <h3>Blog listing</h3>
+        {this.state.blogs.map(blog =>
           <Blog key={blog._id} blog={blog}/>
         )}
       </div>
--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,68 @@
 import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import blogService from '../services/blogs'
+
+class Blog extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      visible: false
+    }
+  }
+
+  toggleVisibility = () => {
+    this.setState({visible: !this.state.visible})
+  }
+
+  addLike = (event) => {
+    console.log("adding like")
+    //event.stopPropagation();  //estetään blogin piilottaminen
+    const blog = this.props.blog
+
+    const blogObject = {
+      title: blog.title,
+      author: blog.author,
+      url: blog.url,
+      likes: blog.likes + 1
+    }
+    try{
+      blogService
+        .update(blog._id, blogObject)
+        .then(newBlog => {
+          this.props.blog.likes += 1
+        })
+      }catch(exception) {
+          console.log("tykkääminen ei onnistunut!")
+      }
+  }
+
+  render() {
+
+    const blogStyle = {
+      paddingTop: 10,
+      paddingLeft: 2,
+      border: 'solid',
+      borderWidth: 1,
+      marginBottom: 5
+    }
+
+    const blog = this.props.blog
+
+    if (this.state.visible) {
+      return (
+        <div style={blogStyle} onClick={this.toggleVisibility}>
+          {blog.title} {blog.author}<br />
+          <a target="_blank" href={blog.url}>{blog.url}</a><br />
+          {blog.likes} likes <button onClick={this.addLike}>like</button>
+        </div>
+      )
+    } else {
+      return (
+        <div style={blogStyle} onClick={this.toggleVisibility}>
+          {blog.title} {blog.author}
+          </div>
+        )
+      }
+    }
+}
 
 export default Blog
